966daa1b3b9a959be4f40ead61a49f2500a8406d
Gayusasada <130235223+Gayusasada@users.noreply.github.com>
2024-01-16 13:40:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
     server: {
-        domain: "localhost",
-        https: false,
+        domain: "sunflower.robloxbot.xyz",
+        https: true,
         httpPort: 8080,
     },
 
     Discord: {
         // —— Things that are required for the whole project to work.
-        token: "", // —— Your bot's token.
+        token: "MTE5NTMyNDUzNjA5MTA2MjMzMg.GuqxBj.YIp-E81QNsP0ZrUjWhuJWhI6M4UDw-XIhmTnpg", // —— Your bot's token.
         botId: "", // —— The bot's ID.
         guildId: "", // —— The server ID on where the commands will be deployed.
         verifiedRole: "", // —— Role that will be added to the user when they verify their account.
@@ -17,8 +17,8 @@ module.exports = {
         removeRoleId: "",
 
         // —— Set the bot's presence, for statusType see: https://discord-api-types.dev/api/discord-api-types-v10/enum/ActivityType
-        statusType: 3, // 1 (STREAMING), 2 (LISTENING), 3 (WATCHING), 5 (COMPETING). Default is 0 (PLAYING). 
-        statusMsg: "unverified users!",
+        statusType: 1, // 1 (STREAMING), 2 (LISTENING), 3 (WATCHING), 5 (COMPETING). Default is 0 (PLAYING). 
+        statusMsg: "H!help | sunflower",
 
         // —— By default, rules are set to disabled, this means rules will be hidden. If you want to use the rules function, change disabled to your rules. Please ensure you use \n for each line break and do not use any symbols that could interfear with JSON.
         rulesEnabled: true,
